ab811d71145544a10df21fe23d8072dc242d8c8b
BlinovA25 <antoha2550@mail.ru>
2021-10-30 15:41:19 +0300

    Fixed version without parsing many weight values

--- main.rb
+++ main.rb
@@ -3,14 +3,14 @@ require 'nokogiri'
 require 'csv'
 
 def enter
-  r = true
-  while(r) do
+  status = true
+  while(status) do
     puts "Enter the link of webpage:"
     url = gets.chomp
     puts "Your link -- url\nEnter Y to continue/N to rewrite"
-    res = gets.chomp
-    if res == 'Y'
-      r = false
+    agree = gets.chomp
+    if agree == 'Y'
+      status = false
       return url
     end
   end
@@ -44,13 +44,47 @@ def create_file (filename)
   puts "File #{filename} was successfully created!"
 end
 
-def one_page_parsing(filename, page_url)
+#<ul class="attribute_radio_list pundaline-variations">
+
+=begin
+def one_product_page_parsing(filename, page_url)
   parsed_page = parsing_func(page_url + ".html")
   finded_name = parsed_page.xpath("//*[@class = 'product_main_name']/text()")#
-  finded_weigth = parsed_page.xpath("//*[@class = 'radio_label']/text()")
+  finded_picture = parsed_page.xpath("//*[@id = 'bigpic']/@src")
+  #<span class="radio_label">6x300Grs</span>
+  #
+  # <li class="quantityDiscount_21300" data-real-discount-value="14.39"
+  # data-discount-type="percentage" data-discount="2" data-discount-quantity="2">
+  # <span class="variation-name"><span>x</span>2</span> <span class="variation-price">28.78 €</span>
+  # <span class="variation-unit-price">14.39 €</span> <span class="variation-discount">- 0.59 € </span>
+  # </li>
+  #
+  parsed_page.xpath("//*[@class = 'attribute_radio_list pundaline-variations']/@li").each { |li_object|
+    finded_price = parsed_page.xpath('//span[contains(@class,"variation-price")]')[0].text #//*[@class = 'price_comb']/text()")
+    finded_weigth = parsed_page.xpath('//span[contains(@class,"variation-name")]')[0].text
+    add_to_file(filename, finded_name.to_s + " " + finded_weigth.to_s, finded_price, finded_picture)
+  }
+=end
+
+
+def one_product_page_parsing(filename, page_url)
+  parsed_page = parsing_func(page_url + ".html")
+  finded_name = parsed_page.xpath("//*[@class = 'product_main_name']/text()")#
   finded_price = parsed_page.xpath("//*[@class = 'price_comb']/text()")
+  finded_weigth = parsed_page.xpath("//*[@class = 'radio_label']/text()")
   finded_picture = parsed_page.xpath("//*[@id = 'bigpic']/@src")
   add_to_file(filename, finded_name.to_s + " " + finded_weigth.to_s, finded_price, finded_picture)
+end
+
+
+def one_site_page_parsing(filename, link_to_some_category)
+  parsed_page = parsing_func(link_to_some_category)
+  find_link_by_class = parsed_page.xpath("//*[@class = 'product_img_link pro_img_hover_scale product-list-category-img']/@href")
+  link_list = find_link_by_class.to_s.split(/.html/)
+  puts "Start parsing products pages:"
+  link_list.each { |url_in_link_list|
+    one_product_page_parsing(filename, url_in_link_list)
+  }
 end
 
 def main
@@ -59,19 +93,22 @@ def main
   link_to_some_category = enter
   puts "Program starts it's work!"
   #link_to_some_category = "https://www.petsonic.com/comida-humeda-para-perros/"
-  parsed_page = parsing_func(link_to_some_category)
-
   create_file(filename)
 
-  find_link_by_class = parsed_page.xpath("//*[@class = 'product_img_link pro_img_hover_scale product-list-category-img']/@href")
-  link_list = find_link_by_class.to_s.split(/.html/)
-
-  puts "Start parsing products pages:"
-  link_list.each { |url_in_link_list|
-    one_page_parsing(filename, url_in_link_list)
-    #puts "#{url_in_link_list} was parsed"
+  check_link = ""
+  (1..100).each { |page_num|
+    result_link_to_category = link_to_some_category + "?p=#{page_num}"
+    if(result_link_to_category == check_link)
+      puts "Program finished it's work!"
+      return
+    end
+    one_site_page_parsing(filename, result_link_to_category)
+    check_link = result_link_to_category
   }
-  puts "Program finished it's work!"
+
 end
+
+#create_file("TestParse.csv")
+#one_product_page_parsing("TestParse.csv", "https://www.petsonic.com/farmina-nd-dog-quinoa-skin-coat-arenque-comida-humeda-para-perros-6x285grs")
 
 main
