761c88217bc5aacdfb3dff3b23ca33dac805552f
BlinovA25 <antoha2550@mail.ru>
2021-11-04 13:54:27 +0300

    Fixed Product class

--- product.rb
+++ product.rb
@@ -1,14 +1,13 @@
 class Product
   attr_accessor :name, :price, :picture
 
-  def initialize(a,b,c)
-    @name = a
-    @price = b
-    @picture = c
+  def initialize(input_name,input_price,input_picture)
+    @name = input_name
+    @price = input_price
+    @picture = input_picture
   end
 
   def show
     puts "Product: #{name}/#{price}/#{picture}"
-
   end
 end
